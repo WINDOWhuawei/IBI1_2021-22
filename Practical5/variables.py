e0261ad92dad758a40af4dfc79a2013b1886b694
unknown <3210110149bit@10.105.100.153git commit -m Initialisegit log --onlinegit commit -m Initialisegit config --global user.email 3210110149bit@10.105.100.153>
2022-03-16 14:54:39 +0800

    edit variables.py

---
+++ Practical5/variables.py
@@ -0,0 +1,14 @@
+xa=19245301
+b=4218520
+c=271
+d=b-c
+e=a-b
+print (d)
+print (e)
+#d=4218249,e=15026781,e is greater. For the rate, we calculate d/c and e/b, and we get the rate of new cases greater in 2020.
+X= True
+Y= False
+W= X and Y
+print (W)
+
+
