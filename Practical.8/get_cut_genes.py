aea30e1837b43a6dc4dc8d538abe2a91ff9014bb
WINDOWhuawei <101187761+WINDOWhuawei@users.noreply.github.com>
2022-04-22 17:28:53 +0800

    Update get_cut_genes.py

--- Practical.8/get_cut_genes.py
+++ Practical.8/get_cut_genes.py
@@ -4,21 +4,21 @@ xfile = open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa')
 fout=open('cut_genes.fa','w')
 y=''
 for line in xfile:
-    if  line.startswith('>'):
-            if 'GAATTC' in y:
+    if  line.startswith('>'):#find out the gene sequence
+            if 'GAATTC' in y:#check if recognition sequence of restriction endonuclease in the sequence
                   #print('gene name:', (x), '   lengeth:', len(y))
-                  font=open('cut_genes.fa','a')
+                  font=open('cut_genes.fa','a')#write the gene name,length,sequence into cut_gene.fa
                   fout.write('>'+str(x))
                   fout.write(str(len(y))+'\n')
                   fout.write(y+'\n')
-            y=''
-            x=str((re.findall(r'gene:(\S+)',line)))
-    if  re.match(r'A|T|C|G',line):
-            y=str(y+line)
-            y=str(''.join(y.split()))
+            y=''#reset y
+            x=str((re.findall(r'gene:(\S+)',line)))# gain the gene name.
+    if  re.match(r'A|T|C|G',line):#find the begain of base sequence.
+            y=str(y+line)#Put the base sequence in y
+            y=str(''.join(y.split()))#remove the space in y
 fout.close
 yfile=open('cut_genes.fa')
-for line in yfile:
+for line in yfile:#open the new-write file.
     print (line[:-1])
 
 
