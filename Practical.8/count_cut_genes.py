a1c119461751755876eb1c98a0ecd2def5c7fdd3
WINDOWhuawei <101187761+WINDOWhuawei@users.noreply.github.com>
2022-04-22 17:20:36 +0800

    Update count_cut_genes.py

--- Practical.8/count_cut_genes.py
+++ Practical.8/count_cut_genes.py
@@ -3,12 +3,12 @@ fname=input('Enter the file name:')
 xfile=open(fname)
 y=''
 for line in xfile:
-    if line.startswith('>'):
-        if 'GAATTC' in y:
-            z = re.findall(r'\S+?GAATTC',y)
+    if line.startswith('>'):#Find each gene segment
+        if 'GAATTC' in y:#Search for the recognition sequence of restriction endonuclease
+            z = re.findall(r'\S+?GAATTC',y)#Use re.find to calculate the cuts
             print('gene name:', (x),'number of fragments:',str((len(z)+1)),'   ', (y))
-        y=''
-        x = str((re.findall(r'gene:(\S+)', line)))
-    if re.match(r'A|T|C|G', line):
+        y=''#reset y
+        x = str((re.findall(r'gene:(\S+)', line)))#gain the gene name
+    if re.match(r'A|T|C|G', line):#use y to get out the base sequence
         y = str(y + line)
-        y = str(''.join(y.split()))
+        y = str(''.join(y.split()))#Remove the Spaces in y
