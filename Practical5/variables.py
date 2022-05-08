cfc75cde4795d0443b6d11469dfba3632997d034
WINDOWhuawei <101187761+WINDOWhuawei@users.noreply.github.com>
2022-05-08 14:06:16 +0800

    Update variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,14 +1,26 @@
-xa=19245301
+a=19245301
 b=4218520
 c=271
 d=b-c
 e=a-b
 print (d)
 print (e)
+print(e>d)#The result is true,so e is greater
+print(d>e)#The result is false
 #d=4218249,e=15026781,e is greater. For the rate, we calculate d/c and e/b, and we get the rate of new cases greater in 2020.
 X= True
 Y= False
 W= X and Y
 print (W)
-
+X=True
+Y=True
+W=X and Y
+print(W)
+X=False
+Y=False
+W=X and Y
+print(W)
+X=False
+Y=True
+print(W)#W should be true only when both X and Y are true
 
